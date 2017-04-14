bd2ab391324c97ce4a09e0a2528cf2c4f86148d7
Philippe Masset <pioul06+github@gmail.com>
2017-04-14 17:41:36 +0200

    Monkeypatch issue w/ pre-populated mentions blocking input
    Without this hack, initiating the editor with text that contains plain text mentions will result in the plugin hijacking all input, effectively blocking all interactions

--- draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
+++ draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
@@ -4,8 +4,8 @@ import findWithRegex from 'find-with-regex';
 import escapeRegExp from 'lodash.escaperegexp';
 
 export default (trigger: string, regExp: string) => (contentBlock: Object, callback: Function) => {
-  const reg = new RegExp(String.raw({
-    raw: `(\\s|^)${escapeRegExp(trigger)}${regExp}` // eslint-disable-line no-useless-escape
-  }), 'g');
-  findWithRegex(reg, contentBlock, callback);
+  findWithRegex(new RegExp(`(\\s|^)${escapeRegExp(trigger)}${regExp}`, 'g'), contentBlock, (start, end) => {
+    const isAlreadyEntity = contentBlock.getEntityAt(Math.max(end - 1, 0)) !== null;
+    if (!isAlreadyEntity) callback(start, end);
+  });
 };
